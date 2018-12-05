9ffba7baac9a74f53272aff36100da79d92f8279
Dylan Leard <dleard@uvic.ca>
2018-12-05 04:48:06 +0000

    Added ingredients problem

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+var repeat = 0;
+while (repeat < ingredients.length) {
+  console.log(ingredients[repeat]);
+  repeat++;
+}
+
+console.log();
+// Write a for loop that prints out the contents of ingredients:
+
+for (let i = 0; i < ingredients.length; i++) console.log(ingredients[i]);
+
+console.log();
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (let i = ingredients.length-1; i >= 0; i--) console.log(ingredients[i]);
